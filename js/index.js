fc20ea329dbfe0a7b1acead0135c3080b17318e7
Aleandro Eccel <aleandro.eccel@gmail.com>
2022-05-05 13:46:35 +0200

    Linked the spicy facts page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 d3.select("#mapAccess").on("click", function () {
     window.location = "map.html";
 });
-d3.select("#Option2").on("click", function () {
-    window.confirm("Option 2");
+d3.select("#spicyFacts").on("click", function () {
+    window.location = "spicy_facts.html";
 });
