6a7199eefd335afee1ada37da7fedf024e8ed01d
noyt <arnaudemmanuel.robert@epfl.ch>
2022-06-03 13:11:43 +0200

    Made compatible with github pages

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 d3.select("#mapAccess").on("click", function () {
-    window.location = "map.html";
+    window.location = "html/map.html";
 });
 d3.select("#spicyFacts").on("click", function () {
-    window.location = "spicy_facts.html";
+    window.location = "html/spicy_facts.html";
 });
 d3.select("#pubHistory").on("click", function () {
-    window.location = "pubHistory.html";
+    window.location = "html/pubHistory.html";
 });
